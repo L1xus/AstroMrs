63ea234924b6099e36217d2a7e948075b885977f
L1xus <omarstellar456@gmail.com>
2024-09-10 14:38:14 +0100

    root folder added to the airflow path

--- dags/movies_dag.py
+++ dags/movies_dag.py
@@ -1,6 +1,9 @@
+import sys
+import os
+from src.fetch_movies import fetch_movies
+
 from datetime import datetime, timedelta
 import requests
-import os
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -20,52 +23,51 @@ default_args = {
     'on_failure_callback': on_failure_callback
 }
 
-def fetch_movies(ti):
-    api_url = ti.xcom_pull(task_ids='something', key='api_url')
-    authorization_key = ti.xcom_pull(task_ids='something', key='authorization_key')
-    endpoint = ti.xcom_pull(task_ids='something', key='endpoint')
-    page = ti.xcom_pull(task_ids='something', key='page')
-
-    page_movies = []
-    
-    headers = {
-        "accept": "application/json",
-        "Authorization": f"Bearer {authorization_key}"
-    }
-
-    if page:
-        page_url = f"{api_url}{endpoint}?language=en-US&page={page}"
-    else:
-        page_url = f"{api_url}{endpoint}"
-
-    response = requests.get(page_url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        if endpoint == '/movie/latest':
-            return [data]
-        else:
-            movies = data.get('results', [])
-            page_movies.extend(movies)
-            print(f"You fetched {page_movies}")
-            return page_movies
-    else:
-        print(f"Failed to fetch page {page}: {response.status_code}")
-        return []
-
-def something(ti):
-    ti.xcom_push(key='api_url', value=API_URL)
-    ti.xcom_push(key='authorization_key', value=AUTHORIZATION_KEY)
-    ti.xcom_push(key='endpoint', value='/movie/popular')
-    ti.xcom_push(key='page', value=1)
-
+# def fetch_movies(ti):
+#     api_url = ti.xcom_pull(task_ids='something', key='api_url')
+#     authorization_key = ti.xcom_pull(task_ids='something', key='authorization_key')
+#     endpoint = ti.xcom_pull(task_ids='something', key='endpoint')
+#     page = ti.xcom_pull(task_ids='something', key='page')
+#
+#     page_movies = []
+#
+#     headers = {
+#         "accept": "application/json",
+#         "Authorization": f"Bearer {authorization_key}"
+#     }
+#
+#     if page:
+#         page_url = f"{api_url}{endpoint}?language=en-US&page={page}"
+#     else:
+#         page_url = f"{api_url}{endpoint}"
+#
+#     response = requests.get(page_url, headers=headers)
+#
+#     if response.status_code == 200:
+#         data = response.json()
+#         if endpoint == '/movie/latest':
+#             return [data]
+#         else:
+#             movies = data.get('results', [])
+#             page_movies.extend(movies)
+#             print(f"You fetched {page_movies}")
+#             return page_movies
+#     else:
+#         print(f"Failed to fetch page {page}: {response.status_code}")
+#         return []
+#
+# def something(ti):
+#     ti.xcom_push(key='api_url', value=API_URL)
+#     ti.xcom_push(key='authorization_key', value=AUTHORIZATION_KEY)
+#     ti.xcom_push(key='endpoint', value='/movie/popular')
+#     ti.xcom_push(key='page', value=1)
+#
 
 with DAG(
-    dag_id="movies_pipeline_v01",
+    dag_id="movies_pipeline_v02",
     default_args=default_args,
     schedule_interval='@daily',
-    start_date=datetime(2024,9,7),
-    catchup=True,
+    start_date=datetime(2024,9,10),
     tags= ["fetch"]
 ) as dag:
 
@@ -75,15 +77,21 @@ with DAG(
     #     headers={"Content-Type": "application/json"},
     #     dag=dag
     # )
-    setup_task = PythonOperator(
-        task_id='something',
-        python_callable=something,
-        dag=dag
-    )
+    # setup_task = PythonOperator(
+    #     task_id='something',
+    #     python_callable=something,
+    #     dag=dag
+    # )
     fetch_task = PythonOperator(
         task_id='fetch_movies',
         python_callable=fetch_movies,
+        op_kwargs={
+            'api_url':API_URL, 
+            'authorization_key':AUTHORIZATION_KEY, 
+            'endpoint':'/movie/popular', 
+            'page':1
+        },
         dag=dag
     )
 
-    setup_task >> fetch_task
+    fetch_task
