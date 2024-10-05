4826c3e4988fd30d4096e3ac4aaef659b4f5d960
L1xus <omarstellar456@gmail.com>
2024-10-05 14:57:09 +0100

    prevent save_movies_mongo from saving duplicates

--- dags/movies_dag.py
+++ dags/movies_dag.py
@@ -5,62 +5,33 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
-from src.fetch_movies import fetch_movies
-from src.transform_movies import validation_aka_transformation
+api_url = os.environ.get("API_URL")
+authorization_key = os.environ.get("AUTHORIZATION_KEY")
 
-api_url = os.environ.get('API_URL')
-authorization_key = os.environ.get('AUTHORIZATION_KEY')
 
 def on_failure_callback(**context):
     print(f"Task {context['task_instance_key_str']} failed.")
 
+
 default_args = {
-    'owner': 'omar',
-    'retries': 3,
-    'retry_delay': timedelta(minutes=5),
-    'on_failure_callback': on_failure_callback
+    "owner": "omar",
+    "retries": 3,
+    "retry_delay": timedelta(minutes=5),
+    "on_failure_callback": on_failure_callback,
 }
 
-# def fetch_movies_task(**kwargs):
-#     page = 1
-#     endpoint = '/movie/popular'
-#     movies = fetch_movies(api_url, authorization_key, endpoint, page)
-#     return movies
-#
-# def transform_movies_task(**kwargs):
-#     ti = kwargs['ti']
-#     fetched_movies = ti.xcom_pull(task_ids='fetch_movies')
-#     transform_movies=validation_aka_transformation(fetched_movies)
-#     return transform_movies
-
 with DAG(
-    dag_id="movies_pipeline_v05",
+    dag_id="movies_pipeline_v06",
     default_args=default_args,
-    schedule_interval='@daily',
-    start_date=datetime(2024,9,26),
-    tags= ["fetch"]
+    schedule_interval="0 15 * * 1",
+    start_date=datetime(2024, 10, 5),
+    tags=["etl"],
 ) as dag:
-
-    # fetch_task = PythonOperator(
-    #     task_id='fetch_movies',
-    #     python_callable=fetch_movies_task,
-    #     provide_context=True,
-    #     dag=dag
-    # )
-    #
-    # transform_task = PythonOperator(
-    #     task_id='transform_movies',
-    #     python_callable=transform_movies_task,
-    #     provide_context=True,
-    #     dag=dag
-    # )
-
     spark_task = SparkSubmitOperator(
-        task_id='movies_etl',
-        application='/opt/airflow/AstroMRS/movies.py',
-        conn_id='spark_conn',
-        dag=dag
+        task_id="movies_etl",
+        application="/opt/airflow/AstroMRS/movies.py",
+        conn_id="spark_conn",
+        dag=dag,
     )
 
-    # fetch_task >> transform_task
     spark_task
--- src/store_movies.py
+++ src/store_movies.py
@@ -1,30 +1,36 @@
-import json
+# import json
 from pymongo import MongoClient
 
-def check_dup(file_path):
-    ids = []
-    with open(file_path, 'r', encoding='utf-8') as f:
-        for line in f:
-            movie = json.loads(line)
-            ids.append(movie['id'])
-    return ids
 
-def save_movies_jsonl(movies, filename):
-    movie_ids = check_dup(filename)
-    with open(filename, 'a', encoding='utf-8') as file: 
-        for movie in movies:
-            if movie['id'] not in movie_ids:   
-                file.write(json.dumps(movie, ensure_ascii=False) + '\n')
-                movie_ids.append(movie['id']) 
-    print(f"{len(movies)} new Movies appended to {filename}")
+# def check_dup(file_path):
+#     ids = []
+#     with open(file_path, "r", encoding="utf-8") as f:
+#         for line in f:
+#             movie = json.loads(line)
+#             ids.append(movie["id"])
+#     return ids
+#
+#
+# def save_movies_jsonl(movies, filename):
+#     movie_ids = check_dup(filename)
+#     with open(filename, "a", encoding="utf-8") as file:
+#         for movie in movies:
+#             if movie["id"] not in movie_ids:
+#                 file.write(json.dumps(movie, ensure_ascii=False) + "\n")
+#                 movie_ids.append(movie["id"])
+#     print(f"{len(movies)} new Movies appended to {filename}")
+
 
 def save_movies_mongo(movies, collection_name):
-    client = MongoClient('mongodb://mongodb:27017/')
-    db = client['movie_database']
+    client = MongoClient("mongodb://mongodb:27017/")
+    db = client["movie_database"]
     collection = db[collection_name]
 
     if movies:
-        collection.insert_many(movies)
+        for movie in movies:
+            existing_movie = collection.find_one({"id": movie["id"]})
+            if not existing_movie:
+                collection.insert_one(movie)
         print(f"Movies saved to MongoDb Collection {collection_name}")
     else:
         print(f"No movies to save to MongoDB collection '{collection_name}'")
